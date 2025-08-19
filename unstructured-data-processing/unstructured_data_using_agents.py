8c86989a766cd53bc17489b10fd5b6697fc9b432
Sankar Biswas <sankarb475@gmail.com>
2025-08-19 12:25:07 +0530

    Create unstructured_data_using_agents.py

---
+++ unstructured-data-processing/unstructured_data_using_agents.py
@@ -0,0 +1,38 @@
+We can create multiple agents to extract the content out of unstructured documents - 
+1) inspection agent 
+-- deep file inspection
+-- bird's eye view of the file 
+-- checksum check 
+-- file type
+-- file content check 
+-- punctuation check 
+-- words spacing 
+-- file size, and legth 
+
+
+2) OCR agent 
+-- use OCR to extract the content out of the documents 
+-- could be an OCR engine, or a multimodal LLM 
+
+
+3) vectorise agent 
+-- chunk up document 
+-- run through LLM 
+
+
+4) Splitter agent 
+-- overlooks the entire process till now 
+-- and decides where document should be split 
+
+
+5) Extract agent 
+-- help us identify key data points 
+-- automated prompting 
+
+ 
+
+6) Match agent 
+-- establish hierarchies 
+-- understand the doc in horizontal or vertical way 
+
+
